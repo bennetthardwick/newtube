08ca9daf78493dd384d8d64a63561c083def9747
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2017-11-06 07:55:01 +0000

    Clicking logo takes you to sub page

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
+// When you manually type in the url to the page
 if(window.location.hash !== "#home" && window.location.pathname === "/")
 	window.location.replace("/feed/subscriptions");
+
+// When you click the logo button it'll take you to your subscriptions page
+// instead
+document.getElementById("logo").addEventListener('click', () => {
+	window.location.replace('/feed/subscriptions');
+});
+
