4d9456775ae4d3826ad7d45011ae862f5386bfb7
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2017-11-07 20:39:00 +0000

    Changed the method of redirect

--- content.js
+++ content.js
@@ -1,7 +1,3 @@
-// When you manually type in the url to the page
-if(window.location.hash !== "#home" && window.location.pathname === "/")
-	window.location.replace("/feed/subscriptions");
-
 // When you click the logo button it'll take you to your subscriptions page
 // instead
 document.getElementById("logo").addEventListener('click', () => {
